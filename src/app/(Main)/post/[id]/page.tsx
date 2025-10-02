3b15d04ea84cba2a7a1f42c87a4fdc449f4ab150
MostafaFathiAbdElAal <mostafafortnite10@gmail.com>
2025-10-02 22:44:09 +0300

    delete isVaild() form

--- src/app/(Main)/post/[id]/page.tsx
+++ src/app/(Main)/post/[id]/page.tsx
@@ -1,43 +1,42 @@
-import { getPost } from "@/actions/getPost.action"
-import PostCard from "@/components/PostCard/PostCard"
-import { SinglePostResponse } from "@/types/posts.type"
-import { Metadata } from "next"
-import { cache } from "react"
-interface pageProps {
-    params: Promise<{ id: string }>
+import { getPost } from "@/actions/getPost.action";
+import PostCard from "@/components/PostCard/PostCard";
+import { SinglePostResponse } from "@/types/posts.type";
+import { Metadata } from "next";
+import { cache } from "react";
+
+interface PageProps {
+  params: { id: string }
 }
 
+export const revalidate = 43200;
 
+const getDetailPost = cache(getPost);
 
-
-export const revalidate = 43200
-
-const getDetailPost = cache(getPost)
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-    const { id } = await params
-    const postId = decodeURIComponent(id)
-    const res: SinglePostResponse = await getDetailPost(postId)
-    return {
-        title: `${res.post.user.name} post`,
-        description: `${res.post.body}`,
-        openGraph: {
-            title: `${res.post.user.name} Post`,
-            description: `${res.post.body}`,
-            images: `${res.post.image ?? ""}`
-        },
-        icons: {
-            icon: `${res.post.user.photo}`
-        }
-    }
+  const postId = decodeURIComponent(params.id);
+  const res: SinglePostResponse = await getDetailPost(postId);
 
+  return {
+    title: `${res.post.user.name} post`,
+    description: res.post.body,
+    openGraph: {
+      title: `${res.post.user.name} Post`,
+      description: res.post.body,
+      images: String(res.post.image),
+    },
+    icons: {
+      icon: String(res.post.user.photo),
+    },
+  };
 }
-export default async function page({ params }: pageProps) {
-    const { id } = await params
-    const postId = decodeURIComponent(id)
-    const res: SinglePostResponse = await getDetailPost(postId)
 
-    return <section className="mt-10">
-        <PostCard post={res.post} />
+export default async function Page({ params }: PageProps) {
+  const postId = decodeURIComponent(params.id);
+  const res: SinglePostResponse = await getDetailPost(postId);
+
+  return (
+    <section className="mt-10">
+      <PostCard post={res.post} />
     </section>
-};
-
+  );
+}
